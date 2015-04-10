419e3f8fd7ee3002c528838d19b496de67428649
eric+firth <ericfirth@appacademy.io>
2015-04-10 15:13:06 -0400

    Phase 2 finished

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -3,10 +3,30 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    keys = []
+    values = []
+
+    params.each do |key, value|
+      keys << key
+      values << value
+    end
+
+    where_line = keys.map {|key| "#{key.to_s} = ?"}.join(" AND ")
+
+    results = DBConnection.execute(<<-SQL, *values)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      WHERE
+        #{where_line}
+      SQL
+
+    results.map{ |result| self.new(result) }
+
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
